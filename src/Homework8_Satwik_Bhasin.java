625f26f77f8651808ac587de9260f5a799bbc6a7
satwikbhasin <satwikbhasin@gmail.com>
2023-05-04 19:56:53 -0400

    changed to command line inputs

--- src/Homework8_Satwik_Bhasin.java
+++ src/Homework8_Satwik_Bhasin.java
@@ -1,5 +1,4 @@
 import java.sql.SQLException;
-import java.util.Scanner;
 
 public class Homework8_Satwik_Bhasin {
     public static void main(String[] args) throws SQLException {
@@ -13,35 +12,38 @@ public class Homework8_Satwik_Bhasin {
          /*creating a new databaseUtility object with the connector as a constructor
          argument which lets us perform actions on the database*/
         databaseUtility utility = new databaseUtility(connector);
+        if(args.length <2){
+            System.out.println("\nError: Table names are empty!");
+        }
+        else {
+            String table1 = args[0].toLowerCase();
+            System.out.print("\n1st table name: " + table1);
+            if (utility.doesTableExist(table1)) {
+                String table2 = args[1].toLowerCase();
+                System.out.println("\n2nd table name: " + table2);
+                if (utility.doesTableExist(table2)) {
 
-        Scanner scanner = new Scanner(System.in);
+                    utility.printTablesInfo(table1);
+                    utility.printTablesInfo(table2);
+                    utility.printIntersectingColumns(table1, table2);
 
-        System.out.print("\nEnter 1st table name: ");
-        String table1 = scanner.nextLine().toLowerCase();
-        if(utility.doesTableExist(table1)) {
-            System.out.print("Enter 2nd table name: ");
-            String table2 = scanner.nextLine().toLowerCase();
-            if (utility.doesTableExist(table2)) {
+                    int estimatedJoinSize = utility.estimateJoinSize(table1, table2);
+                    int actualJoinSize = utility.actualJoinSize(table1, table2);
+                    System.out.println(
+                            "\n--------------------------------------- FINAL SOLUTION ---------------------------------------");
+                    System.out.println("\nEstimated join size: " + estimatedJoinSize);
+                    System.out.println("Actual join size: " + actualJoinSize);
+                    System.out.println("Estimation error: " + (estimatedJoinSize - actualJoinSize));
+                    System.out.println(
+                            "\n----------------------------------------------------------------------------------------------");
 
-                utility.printTablesInfo(table1);
-                utility.printTablesInfo(table2);
-                utility.printIntersectingColumns(table1, table2);
-
-                int estimatedJoinSize = utility.estimateJoinSize(table1, table2);
-                int actualJoinSize = utility.actualJoinSize(table1, table2);
-                System.out.println("\n--------------------------------------- FINAL SOLUTION ---------------------------------------");
-                System.out.println("\nEstimated join size: " + estimatedJoinSize);
-                System.out.println("Actual join size: " + actualJoinSize);
-                System.out.println("Estimation error: " + (estimatedJoinSize - actualJoinSize));
-                System.out.println("\n----------------------------------------------------------------------------------------------");
+                } else {
+                    System.out.println("No table found with name: '" + table2 + "'");
+                }
+            } else {
+                System.out.println("No table found with name: '" + table1 + "'");
             }
-            else {
-                System.out.println("No table found with name: '" + table2 + "'");
-            }
-        } else {
-            System.out.println("No table found with name: '" + table1 + "'");
         }
         connector.disconnect(connector.connection);
-
     }
 }
