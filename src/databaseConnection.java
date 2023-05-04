517ea38e13bf312c40e70c1dd439fe6eb8e46594
satwikbhasin <satwikbhasin@gmail.com>
2023-05-04 16:08:32 -0400

    adding separate variables for database name & port number

--- src/databaseConnection.java
+++ src/databaseConnection.java
@@ -4,13 +4,22 @@ import java.sql.SQLException;
 
 public class databaseConnection {
     String url;
+    String database;
+    String port;
     String username;
     String password;
     Connection connection;
 
     databaseConnection() {
         // postgreSQL database credentials
-        this.url = "jdbc:postgresql://localhost:5432/universityDataset";
+
+        // change this to whatever you have named the university dataset as
+        this.database = "universityDataset";
+
+        // change this to whatever port your postgresql server is working on
+        this.port = "5432";
+
+        this.url = "jdbc:postgresql://localhost:";
         this.username = "postgres";
         this.password = "Thumbsup10";
     }
@@ -18,7 +27,7 @@ public class databaseConnection {
     // establishes the connection to database
     public void connect() {
         try {
-            this.connection = DriverManager.getConnection(url, username, password);
+            this.connection = DriverManager.getConnection(url + port + "/" + database, username, password);
             System.out.println("\nConnected to the university database in PostgreSQL!");
         } catch (SQLException e) {
             System.out.println("\nConnection failure: " + e.getMessage());
